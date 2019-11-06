7e1e71de3b121832d2d291483eee11d9a867b391
M Nkuna (21635155) <musankuna0@gmail.com>
2019-11-06 15:27:41 +0200

    add

---
+++ Models/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppDev3B_AI_Assignment_2019.Models
+{
+    public class Class1
+    {
+    }
+}
